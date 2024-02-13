375ee9e43a5b8ebbbe7b64acb4f9631ba4f52870
Jhymas20 <Jordanhymas24@gmail.com>
2024-02-13 19:33:26 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CS445',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
